183793cfd78f16ae6420d0783be957f62cf87021
nanamori <ms.ops271@gmail.com>
2026-07-10 17:29:43 +0900

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -1,9 +1,7 @@
 export async function onRequestPost(context) {
   try {
-    // 上記のHTMLからプッシュされてきたデータを受け取る
     const data = await context.request.json();
     
-    // Cloudflareのサーバー側で、安全にD1（MY_DB）に流し込む
     await context.env.MY_DB.prepare(
       "INSERT INTO surveys (name, grade, q1, q2, q3, created_at) VALUES (?, ?, ?, ?, ?, ?)"
     )
@@ -12,7 +10,7 @@ export async function onRequestPost(context) {
       data.grade, 
       data.q1_answers.join(","), 
       data.q2_answers.join(","), 
-      data.q3_answers.join(","), 
+      data.q3_answer,
       data.timestamp
     )
     .run();
